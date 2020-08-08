7fa9008f0c9945d639af8fbac11f7b90d1f36cae
Hilmi Kantarci <hilmikantarci@gmail.com>
2020-08-08 18:37:50 +0300

    Add project files.

---
+++ WanderWorld/Infrastructure/Concrete/SessionHelper.cs
@@ -0,0 +1,19 @@
+﻿using WanderWorld.ModalLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WanderWorld.Infrastructure.Concrete
+{
+    public class SessionHelper
+    {
+        public static LoginUser CurrentUser
+        {
+            get
+            {
+                return HttpContext.Current.Session["login"] as LoginUser;
+            }
+        }
+    }
+}
---
+++ WanderWorld/ModalLoginPlugin/ModalLoginJsonResult.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WanderWorld.Infrastructure.Abstract;
+
+namespace WanderWorld.ModalLogin
+{
+    internal partial class ModalLoginJsonResult : MyJsonResult<string>
+    {
+
+    }
+}
